aaa888c95cffbff91d372b5fd2c71e511641de06
lkelsall <kelsall.lewis@gmail.com>
2021-09-09 11:39:51 +0100

    Implement getWeather controller

---
+++ controllers.js
@@ -0,0 +1,10 @@
+const { callOpenWeather } = require("models.js");
+
+exports.getWeather = (req, res, next) => {
+  const { lat, lng } = req.query;
+  callOpenWeather(lat, lng)
+    .then((weather) => {
+      res.status(200).send({ weather: weather });
+    })
+    .catch(next);
+};
